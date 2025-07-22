786a925e5dbb4e97bfa478173ecaead04085b479
coetaur0 <aurelien.coet19@gmail.com>
2025-07-22 13:56:47 +0200

    Implement the parser for expressions

--- src/main/scala/inox/parsing/Parser.scala
+++ src/main/scala/inox/parsing/Parser.scala
@@ -2,16 +2,22 @@ package inox.parsing
 
 import scala.util.control.Breaks
 
-import inox.ast.{Name, Stmt, Type}
+import inox.ast.{BinaryOp, Block, Expr, Name, Type, UnaryOp}
 import inox.{Result, Span, Spanned}
 
 /** A parser for Inox. */
 object Parser {
 
-  /** Parses a source and returns the corresponding AST or a sequence of errors
-    * if parsing fails.
+  /** Parses an expression in a source and returns the corresponding AST or a
+    * sequence of errors if parsing fails.
     */
-  def parse(source: String): Result[Type, ParseError] =
+  def parseExpr(source: String): Result[Expr, ParseError] =
+    Parser(source).expr()
+
+  /** Parses a type expression in a source and returns the corresponding AST or
+    * a sequence of errors if parsing fails.
+    */
+  def parseType(source: String): Result[Type, ParseError] =
     Parser(source).ty()
 }
 
@@ -19,6 +25,183 @@ object Parser {
 private class Parser(source: String) {
   private val lexer = Lexer(source)
   private var token = lexer.next()
+
+  /** Parses a block expression. */
+  private def block(): Result[Spanned[Block], ParseError] = ???
+
+  /** Parses an expression. */
+  private def expr(): Result[Expr, ParseError] =
+    for {
+      lhs <- unaryExpr()
+      expr <- binaryExpr(0, lhs)
+    } yield expr
+
+  /** Parses a binary expression. */
+  private def binaryExpr(prec: Int, lhs: Expr): Result[Expr, ParseError] = {
+    val (op, nextPrec) = token.item match {
+      case Token.PipeX2      => (BinaryOp.Or, 1)
+      case Token.AmpersandX2 => (BinaryOp.And, 2)
+      case Token.EqualX2     => (BinaryOp.Eq, 3)
+      case Token.BangEqual   => (BinaryOp.Neq, 3)
+      case Token.LAngleEqual => (BinaryOp.Le, 4)
+      case Token.RAngleEqual => (BinaryOp.Ge, 4)
+      case Token.LAngle      => (BinaryOp.Lt, 4)
+      case Token.RAngle      => (BinaryOp.Gt, 4)
+      case Token.Plus        => (BinaryOp.Add, 5)
+      case Token.Minus       => (BinaryOp.Sub, 5)
+      case Token.Star        => (BinaryOp.Mul, 6)
+      case Token.Slash       => (BinaryOp.Div, 6)
+      case _                 => (BinaryOp.Or, 0)
+    }
+    if (prec < nextPrec) {
+      advance()
+      for {
+        unary <- unaryExpr()
+        rhs <- binaryExpr(nextPrec, unary)
+        expr <- binaryExpr(
+          prec,
+          Expr.Binary(op, lhs, rhs, Span(lhs.span.start, rhs.span.end))
+        )
+      } yield expr
+    } else
+      Result.Success(lhs)
+  }
+
+  /** Parses a unary expression. */
+  private def unaryExpr(): Result[Expr, ParseError] = {
+    val op = token.item match {
+      case Token.Bang  => Some(UnaryOp.Not)
+      case Token.Minus => Some(UnaryOp.Neg)
+      case _           => None
+    }
+    op match {
+      case Some(op) =>
+        val start = advance().span.start
+        for operand <- unaryExpr()
+        yield Expr.Unary(op, operand, Span(start, operand.span.end))
+      case None =>
+        for {
+          callee <- primaryExpr()
+          call <- callExpr(callee)
+        } yield call
+    }
+  }
+
+  /** Parses a sequence of call expressions. */
+  private def callExpr(callee: Expr): Result[Expr, ParseError] =
+    if (token.item == Token.LParen)
+      for {
+        args <- delimited(
+          () => list(expr, Token.Comma, Token.RParen),
+          Token.LParen,
+          Token.RParen
+        )
+        call <- callExpr(
+          Expr.Call(callee, args.item, Span(callee.span.start, args.span.end))
+        )
+      } yield call
+    else
+      Result.Success(callee)
+
+  /** Parses a primary expression. */
+  private def primaryExpr(): Result[Expr, ParseError] =
+    token.item match {
+      case Token.LParen    => parenExpr()
+      case Token.IfKw      => ifExpr()
+      case Token.Ampersand => borrowExpr()
+      case Token.Name      => varExpr()
+      case Token.LBrace    =>
+        for {
+          body <- block()
+        } yield Expr.Block(body.item, body.span)
+      case Token.IntLit =>
+        val span = advance().span
+        val value = source.substring(span.start.offset, span.end.offset).toInt
+        Result.Success(Expr.IntLit(value, span))
+      case Token.TrueLit  => Result.Success(Expr.BoolLit(true, advance().span))
+      case Token.FalseLit => Result.Success(Expr.BoolLit(false, advance().span))
+      case _              => Result.Failure(expected("an expression"))
+    }
+
+  /** Parses a parenthesised expression. */
+  private def parenExpr(): Result[Expr, ParseError] = {
+    val start = advance().span.start
+    if (token.item == Token.RParen)
+      Result.Success(Expr.Unit(Span(start, advance().span.end)))
+    else
+      for {
+        expr <- expr()
+        _ <- close("(", Token.RParen)
+      } yield expr
+  }
+
+  /** Parses an if expression. */
+  private def ifExpr(): Result[Expr, ParseError] = {
+    def elseExpr(): Result[Expr, ParseError] =
+      if (token.item == Token.ElseKw) {
+        advance()
+        if (token.item == Token.IfKw)
+          ifExpr()
+        else {
+          for els <- block() yield Expr.Block(els.item, els.span)
+        }
+      } else
+        Result.Success(Expr.Unit(token.span))
+
+    val start = advance().span.start
+    for {
+      cond <- expr()
+      thn <- block()
+      els <- elseExpr()
+    } yield Expr.If(cond, thn, els, Span(start, els.span.end))
+  }
+
+  /** Parses a borrow expression. */
+  private def borrowExpr(): Result[Expr, ParseError] = {
+    val start = advance().span.start
+    for {
+      mutable <- mut()
+      expr <- expr()
+    } yield Expr.Borrow(mutable, expr, Span(start, expr.span.end))
+  }
+
+  /** Parses a variable expression. */
+  private def varExpr(): Result[Expr, ParseError] = {
+    def originArg() =
+      token.item match {
+        case Token.Origin =>
+          val origin = text
+          advance()
+          Result.Success(Some(origin))
+        case Token.LocalOrigin =>
+          advance()
+          Result.Success(None)
+        case _ => Result.Failure(expected("an origin"))
+      }
+
+    def originArgs(
+        default: Span
+    ): Result[Spanned[IndexedSeq[Option[Name]]], ParseError] =
+      if (token.item == Token.ColonX2) {
+        advance()
+        for origins <- delimited(
+            () => list(originArg, Token.Comma, Token.RAngle),
+            Token.LAngle,
+            Token.RAngle
+          )
+        yield origins
+      } else
+        Result.Success(Spanned(IndexedSeq[Option[Name]](), default))
+
+    for {
+      name <- consume(Token.Name)
+      origins <- originArgs(name.span)
+    } yield Expr.Var(
+      name,
+      origins.item,
+      Span(name.span.start, origins.span.end)
+    )
+  }
 
   /** Parses a type expression. */
   private def ty(): Result[Type, ParseError] =
--- src/test/scala/inox/parsing/ParserTests.scala
+++ src/test/scala/inox/parsing/ParserTests.scala
@@ -5,14 +5,72 @@ import org.scalatest.funsuite.AnyFunSuite
 import inox.{Location, Result, Span, Spanned}
 
 class ParserTests extends AnyFunSuite {
+  test("Expressions should be properly parsed") {
+    checkOk("(x + 1) * 4 < 10 == c || d", Parser.parseExpr)
+    checkOk("!a && b", Parser.parseExpr)
+    checkOk("!!true", Parser.parseExpr)
+    checkOk("f(42, 19)(true)", Parser.parseExpr)
+    checkOk("&mut 42", Parser.parseExpr)
+    checkOk("x::<'a, '_>", Parser.parseExpr)
+    checkOk("x", Parser.parseExpr)
+    checkOk("42", Parser.parseExpr)
+    checkOk("true", Parser.parseExpr)
+    checkOk("false", Parser.parseExpr)
+    checkOk("()", Parser.parseExpr)
+
+    checkError(
+      "(x + 1",
+      Parser.parseExpr,
+      IndexedSeq(
+        ParseError.UnclosedDelimiter(
+          "(",
+          "a ')'",
+          Spanned("", Span(Location(1, 7, 6), Location(1, 7, 6)))
+        )
+      )
+    )
+    checkError(
+      "&i32",
+      Parser.parseExpr,
+      IndexedSeq(
+        ParseError.UnexpectedSymbol(
+          "an expression",
+          Spanned("i32", Span(Location(1, 2, 1), Location(1, 5, 4)))
+        )
+      )
+    )
+    checkError(
+      "x::",
+      Parser.parseExpr,
+      IndexedSeq(
+        ParseError.UnexpectedSymbol(
+          "a '<'",
+          Spanned("", Span(Location(1, 4, 3), Location(1, 4, 3)))
+        )
+      )
+    )
+    checkError(
+      "x::<'a",
+      Parser.parseExpr,
+      IndexedSeq(
+        ParseError.UnclosedDelimiter(
+          "<",
+          "a '>'",
+          Spanned("", Span(Location(1, 7, 6), Location(1, 7, 6)))
+        )
+      )
+    )
+  }
+
   test("Type expressions should be properly parsed") {
-    checkOk("fn(i32, bool) -> i32")
-    checkOk("&'a mut i32")
-    checkOk("(i32)")
-    checkOk("bool")
+    checkOk("fn(i32, bool) -> i32", Parser.parseType)
+    checkOk("&'a mut i32", Parser.parseType)
+    checkOk("(i32)", Parser.parseType)
+    checkOk("bool", Parser.parseType)
 
     checkError(
       "(i32",
+      Parser.parseType,
       IndexedSeq(
         ParseError.UnclosedDelimiter(
           "(",
@@ -23,6 +81,7 @@ class ParserTests extends AnyFunSuite {
     )
     checkError(
       "fn -> i32",
+      Parser.parseType,
       IndexedSeq(
         ParseError.UnexpectedSymbol(
           "a '('",
@@ -32,6 +91,7 @@ class ParserTests extends AnyFunSuite {
     )
     checkError(
       "&true",
+      Parser.parseType,
       IndexedSeq(
         ParseError.UnexpectedSymbol(
           "a type expression",
@@ -42,17 +102,21 @@ class ParserTests extends AnyFunSuite {
   }
 
   /** Checks that parsing some source string succeeds. */
-  private def checkOk(source: String): Unit =
-    assert(Parser.parse(source).isSuccess)
+  private def checkOk[A](
+      source: String,
+      parse: String => Result[A, ParseError]
+  ): Unit =
+    assert(parse(source).isSuccess)
 
   /** Checks that parsing some source string returns an `expected` sequence of
     * parse errors.
     */
-  private def checkError(
+  private def checkError[A](
       source: String,
+      parse: String => Result[A, ParseError],
       expected: IndexedSeq[ParseError]
   ): Unit =
-    Parser.parse(source) match {
+    parse(source) match {
       case Result.Success(result) =>
         assert(false, "Expected parse errors in the input string.")
       case Result.Failure(errors) =>
